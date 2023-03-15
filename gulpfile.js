942563ccc1d6b4b8690e859963383077fd0f2aca
rafaelranery <rafaelrapnery@gmail.com>
2023-03-15 10:11:56 -0300

    criando o hero e importando a fonte Avenir

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,19 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
-
+const imagemin = require('gulp-imagemin');
 
 function styles() {
     return gulp.src('./src/styles/*.scss')
     .pipe(sass({outputStyle: 'compressed'}))
     .pipe(gulp.dest('./dist/css'));
 }
-
-function watch(e) {
-    
+function images() {
+    return gulp.src('./src/images/**/*')
+    .pipe(imagemin())
+    .pipe(gulp.dest('./dist/image'));
 }
 
-exports.default = styles;
+exports.default = gulp.parallel(styles, images);
 exports.watch = function () {
     gulp.watch('./src/styles/*.scss', {ignoreInitial: false}, gulp.parallel(styles))
 }
