825f026fe26ab55385e3c3a0ab067982f66e7bba
rafaelranery <rafaelrapnery@gmail.com>
2023-03-17 17:36:55 -0300

    config uglify

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,13 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const imagemin = require('gulp-imagemin');
+const uglify = require('gulp-uglify');
+
+function scripts() {
+    return gulp.src('./src/scripts/*.js')
+        .pipe(uglify())
+        .pipe(gulp.dest('./dist/js'))
+}
 
 function styles() {
     return gulp.src('./src/styles/*.scss')
@@ -13,7 +20,9 @@ function images() {
     .pipe(gulp.dest('./dist/image'));
 }
 
-exports.default = gulp.parallel(styles, images);
+exports.default = gulp.parallel(styles, images, scripts);
 exports.watch = function () {
     gulp.watch('./src/styles/*.scss', {ignoreInitial: false}, gulp.parallel(styles))
+    gulp.watch('./src/scripts/*.js', {ignoreInitial: false}, gulp.parallel(scripts))
+
 }
