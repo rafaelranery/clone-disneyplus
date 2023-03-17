a580dbcb0329deb8c3e8d7d1c0f7a70282e17804
rafaelranery <rafaelrapnery@gmail.com>
2023-03-17 17:28:53 -0300

    ajustos no mobile

--- src/main.js
+++ src/main.js
@@ -5,12 +5,19 @@ document.addEventListener('DOMContentLoaded', function() {
     const tabsContainer = document.querySelectorAll('[data-tab-id]');
     const questions =document.querySelectorAll('[data-faq-question]');
     
-    for (let i = 0; i < questions.length; i++) {
-        questions[i].addEventListener('click', abreOuFechaResposta)
-        
-    }
+    const heroSection = document.querySelector('.hero');
+    const alturaHero = heroSection.clientHeight;
     
-    
+    window.addEventListener('scroll', function() {
+        const posicaoAtual = window.scrollY;
+
+        if (posicaoAtual < alturaHero) {
+            ocultarElementosDoHeader();
+            console.log('ocultar elementos')
+        } else {
+            exibeElementosDoHeader();
+        }
+    })
     
     
     
@@ -19,10 +26,10 @@ document.addEventListener('DOMContentLoaded', function() {
     é um array. Ou seja, é necessário aplicar a função a cada elemento.
     Não se trata de todos os elementos de tal classe selecionados (como
     no CSS), mas o retorno de uma lista de elementos */
-    
+    /* Seção atrações, programação das abas */
     for (let i = 0; i < buttons.length; i++) {
         buttons[i].addEventListener('click', function(button) {
-
+            
             const abaAlvo = button.target.dataset.tabButton;
             const aba = document.querySelector(`[data-tab-id=${abaAlvo}]`)
             escondeTodasAbas();
@@ -30,7 +37,23 @@ document.addEventListener('DOMContentLoaded', function() {
             button.target.classList.add('shows__tabs__button--is-active')
         })
     }
+    /* Seção faq, programação do accordion */
+    for (let i = 0; i < questions.length; i++) {
+        questions[i].addEventListener('click', abreOuFechaResposta)
+        
+    }
 })
+function exibeElementosDoHeader() {
+    const header = document.querySelector('.header');
+
+    header.classList.remove('header--is-hidden');
+}
+
+function ocultarElementosDoHeader() {
+    const header = document.querySelector('.header');
+
+    header.classList.add('header--is-hidden');
+}
 
 function abreOuFechaResposta(elemento) {
     const classe = 'faq__questions__item--is-open';
