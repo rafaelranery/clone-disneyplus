d23b0c5d7979e25805adc60ec1520daf068a8ea9
rafaelranery <rafaelrapnery@gmail.com>
2023-03-17 11:32:17 -0300

    criando a seção de planos

---
+++ src/main.js
@@ -0,0 +1,33 @@
+const buttons = document.querySelectorAll('[data-tab-button]');
+document.addEventListener('DOMContentLoaded', function() {
+    
+    console.log(buttons)
+    const tabsContainer = document.querySelectorAll('[data-tab-id]');
+
+    /* é necessário o array pois o retorno do querySelector
+    é um array. Ou seja, é necessário aplicar a função a cada elemento.
+    Não se trata de todos os elementos de tal classe selecionados (como
+    no CSS), mas o retorno de uma lista de elementos */
+    
+    for (let i = 0; i < buttons.length; i++) {
+        buttons[i].addEventListener('click', function(button) {
+
+            const abaAlvo = button.target.dataset.tabButton;
+            const aba = document.querySelector(`[data-tab-id=${abaAlvo}]`)
+            escondeTodasAbas();
+            aba.classList.add('shows__list--is-active')
+            button.target.classList.add('shows__tabs__button--is-active')
+        })
+    }
+})
+
+function escondeTodasAbas() {
+    const tabsContainer = document.querySelectorAll('[data-tab-id]');
+
+    for (let i = 0; i < tabsContainer.length; i++) {
+        tabsContainer[i].classList.remove('shows__list--is-active');
+    }
+    for (let i = 0; i < buttons.length; i++) {
+        buttons[i].classList.remove('shows__tabs__button--is-active');
+    }
+}
